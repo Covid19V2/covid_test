92f1fa89ae9ee1458944f1ed934e676052a43cce
ebxn <ebengtuff@gmail.com>
2020-07-19 14:24:58 +0100

    Init commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h1>How to eat ass.</h1>
+    <ul>
+      <li>Step 1: Remove dem clothes</li>
+      <li>Step 2: Open dat mouth</li>
+      <li>Step 3: Go for it tiger</li>
+    </ul>
+
+    <AssEater />
+  </div>
+</template>
+
+<script>
+import AssEater from './components/AssEater.vue'
+
+export default {
+  name: 'App',
+  components: {
+    AssEater
+  }
+}
+</script>
---
+++ src/components/AssEater.vue
@@ -0,0 +1,18 @@
+<template>
+  <button @click="count++">Eat it</button>
+  <p>Asses eaten: {{ count }}</p>
+</template>
+
+<script>
+export default {
+  name: 'AssEater',
+  props: {
+    msg: String
+  },
+  data() {
+    return {
+      count: 0
+    }
+  }
+}
+</script>
